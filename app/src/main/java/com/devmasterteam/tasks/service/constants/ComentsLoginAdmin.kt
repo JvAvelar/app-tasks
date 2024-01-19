8622fabb64833bce837e36a330170c2e85d1136e
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-01-19 19:50:56 -0300

    Criado uma classe para que seja salvo em comentário as informações do meu login realizado via API.

---
+++ app/src/main/java/com/devmasterteam/tasks/service/constants/ComentsLoginAdmin.kt
@@ -0,0 +1,16 @@
+package com.devmasterteam.tasks.service.constants
+
+class ComentsLoginAdmin private constructor() {
+
+    /*
+    {
+    "name": "Joao ",
+    "token": "8ms6/pOgDQk7Zo/qhenUR8BPWOH76Fs6u1qysn9D1S/SmM2gWcrT0g9Yn8IJPQRa",
+    "personKey": "dydEa2+onzkQyoMgm7Hnx3/FSRpNHXhs84iEYeECFVU="
+}
+
+name "Joao "
+email "jvitoraveelar1200@gmail.com"
+password "170903"
+     */
+}
