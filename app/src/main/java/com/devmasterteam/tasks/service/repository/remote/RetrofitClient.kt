2a774cde50bbfcc610f028b9ea97a3d344e11691
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-01-21 00:30:12 -0300

    Refatoração: Consertando o nome da variavel na classe RetrofitClient.kt e adicionando addInterceptor juntamente com o httpClient

--- app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
@@ -1,6 +1,9 @@
 package com.devmasterteam.tasks.service.repository.remote
 
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,13 +11,25 @@ class RetrofitClient private constructor() {
     companion object {
 
         private lateinit var INSTANCE: Retrofit
+        private  var token: String = ""
+        private  var personKey: String = ""
+
         private fun getRetrofitInstance(): Retrofit {
-            val httpCliente = OkHttpClient.Builder()
+            val httpClient = OkHttpClient.Builder()
+            httpClient.addInterceptor { chain ->
+                val request = chain.request()
+                    .newBuilder()
+                    .addHeader(TaskConstants.HEADER.TOKEN_KEY, token)
+                    .addHeader(TaskConstants.HEADER.PERSON_KEY, personKey)
+                    .build()
+                chain.proceed(request)
+            }
+
             if (!::INSTANCE.isInitialized) {
                 synchronized(RetrofitClient::class) {
                     INSTANCE = Retrofit.Builder()
                         .baseUrl("http://devmasterteam.com/CursoAndroidAPI/")
-                        .client(httpCliente.build())
+                        .client(httpClient.build())
                         .addConverterFactory(GsonConverterFactory.create())
                         .build()
                 }
@@ -25,5 +40,13 @@ class RetrofitClient private constructor() {
         fun <T> getService(serviceClass: Class<T>) : T {
             return getRetrofitInstance().create(serviceClass)
         }
+
+        fun addHeaders(tokenValue: String, personKeyValue: String){
+            token = tokenValue
+            personKey = personKeyValue
+
+
+        }
+
     }
 }
