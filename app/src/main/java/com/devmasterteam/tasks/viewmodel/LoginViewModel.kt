97a60978eb17d139d391ca4b8220820e924add28
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-01-21 20:30:00 -0300

    Refatoração: Implementada novas regras de negócio

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -7,15 +7,19 @@ import androidx.lifecycle.MutableLiveData
 import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
+import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
+import com.devmasterteam.tasks.service.repository.PriorityRepository
 import com.devmasterteam.tasks.service.repository.SecurityPreferences
+import com.devmasterteam.tasks.service.repository.local.PriorityDAO
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val personRepository = PersonRepository(application.applicationContext)
     private val securityPreferences = SecurityPreferences(application.applicationContext)
+    private val priorityRepository = PriorityRepository(application.applicationContext)
 
     private val _login = MutableLiveData<ValidationModel>()
     val login: LiveData<ValidationModel> = _login
@@ -33,7 +37,9 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
                 securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, result.token)
                 securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, result.personKey)
                 securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, result.name)
+
                 RetrofitClient.addHeaders(result.token, result.personKey)
+
                 _login.value = ValidationModel()
             }
 
@@ -50,7 +56,21 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
         val token = securityPreferences.get(TaskConstants.SHARED.TOKEN_KEY)
         val person = securityPreferences.get(TaskConstants.SHARED.PERSON_KEY)
         RetrofitClient.addHeaders(token, person)
-        _loggedUser.value = (token != "" && person != "")
+        val logged = (token != "" && person != "")
+        _loggedUser.value = logged
+
+        if (!logged) {
+            priorityRepository.list(object : APIListener<List<PriorityModel>> {
+                override fun onSuccess(result: List<PriorityModel>) {
+                    val s = ""
+                }
+
+                override fun onFailure(message: String) {
+                    val s = ""
+                }
+
+            })
+        }
     }
 
 }
