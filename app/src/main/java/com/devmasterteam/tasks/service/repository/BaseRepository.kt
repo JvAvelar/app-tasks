a60e44d064b7163a51eeac0fcd116b33db0de5db
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-01-27 12:34:56 -0300

    Criação: Criada a classe BaseRepository.kt para que possamos herdar métodos que a ela pertencem, a fim de não repetir código.

--- app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
@@ -1,11 +1,28 @@
 package com.devmasterteam.tasks.service.repository
 
+import android.content.Context
+import com.devmasterteam.tasks.R
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import com.devmasterteam.tasks.service.listener.APIListener
 import com.google.gson.Gson
+import retrofit2.Response
+import java.util.Objects
 
 open class BaseRepository {
 
 
-    fun failResponse(str: String): String {
+    private fun failResponse(str: String): String {
         return Gson().fromJson(str, String::class.java)
+    }
+
+    fun <T> handleResponse(response: Response<T>, listener: APIListener<T>) {
+        if (response.code() == TaskConstants.HTTP.SUCCESS)
+            response.body()?.let { listener.onSuccess(it) }
+        else
+            listener.onFailure(failResponse(response.errorBody()!!.string()))
+    }
+
+    fun <T> failureError(listener: APIListener<T>, context: Context) {
+        listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
     }
 }
