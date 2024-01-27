9499b00ade4bad0810d744e2a2ca621a3c5e95c6
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-01-27 12:56:25 -0300

    Implementação: Implementada novas funcionalidades e uma pequena refatoração na classe TaskFormActivity.kt

--- app/src/main/java/com/devmasterteam/tasks/view/TaskFormActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/TaskFormActivity.kt
@@ -7,9 +7,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.DatePicker
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.databinding.ActivityTaskFormBinding
+import com.devmasterteam.tasks.service.model.PriorityModel
+import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.viewmodel.TaskFormViewModel
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -22,6 +25,7 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
 
     @SuppressLint("SimpleDateFormat")
     private val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+    private var listPriority: List<PriorityModel> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +49,9 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
     override fun onClick(v: View) {
         if (v.id == R.id.button_date) {
             handleDate()
+        } else if (v.id == R.id.button_save) {
+            handleSave()
+
         }
     }
 
@@ -63,14 +70,40 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
         DatePickerDialog(this, this, year, month, day).show()
     }
 
-    private fun observe(){
-        viewModel.priorityList.observe(this){
+    private fun handleSave() {
+        val task = TaskModel().apply {
+            this.id = 0
+            val index = binding.spinnerPriority.selectedItemPosition
+            this.priorityId = listPriority[index].id
+            this.description = binding.editDescription.text.toString()
+            this.dueDate = binding.buttonDate.text.toString()
+            this.complete = binding.checkComplete.isChecked
+        }
+        viewModel.save(task)
+    }
+
+    private fun toast(str: String) {
+        Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun observe() {
+        viewModel.priorityList.observe(this) {
+            listPriority = it
             val list = mutableListOf<String>()
-            for (p in it){
+            for (p in it) {
                 list.add(p.description)
             }
             val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list)
             binding.spinnerPriority.adapter = adapter
+        }
+
+        viewModel.taskSave.observe(this) {
+            if (it.status()) {
+                toast("Sucesso")
+                finish()
+            } else {
+                toast(it.message())
+            }
         }
     }
 }
