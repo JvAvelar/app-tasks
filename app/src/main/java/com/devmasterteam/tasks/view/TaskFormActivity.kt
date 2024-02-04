2df7224068fda81e0cf481af0c9cac4075930b62
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-02-04 16:22:56 -0300

    Implementação: Adicionado um novo método loadDataFromActivity e getIndex na classe TaskFormActivity.kt

--- app/src/main/java/com/devmasterteam/tasks/view/TaskFormActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/TaskFormActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.databinding.ActivityTaskFormBinding
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.viewmodel.TaskFormViewModel
@@ -26,6 +27,7 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
     @SuppressLint("SimpleDateFormat")
     private val dateFormat = SimpleDateFormat("dd/MM/yyyy")
     private var listPriority: List<PriorityModel> = mutableListOf()
+    private var taskIdentification = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +41,8 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
         binding.buttonDate.setOnClickListener(this)
 
         viewModel.loadPriorities()
+
+        loadDataFromActivity()
 
         observe()
 
@@ -62,6 +66,14 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
         binding.buttonDate.text = dueDate
     }
 
+    private fun loadDataFromActivity() {
+        val bundle = intent.extras
+        if (bundle != null) {
+             taskIdentification = bundle.getInt(TaskConstants.BUNDLE.TASKID)
+            viewModel.load(taskIdentification)
+        }
+    }
+
     private fun handleDate() {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
@@ -72,7 +84,7 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
 
     private fun handleSave() {
         val task = TaskModel().apply {
-            this.id = 0
+            this.id = taskIdentification
             val index = binding.spinnerPriority.selectedItemPosition
             this.priorityId = listPriority[index].id
             this.description = binding.editDescription.text.toString()
@@ -86,6 +98,18 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
         Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
     }
 
+    private fun getIndex(priorityId : Int) : Int{
+        var index = 0
+        for(l in listPriority){
+            if(l.id == priorityId){
+                break
+            }
+            index++
+        }
+        return index
+    }
+
+    @SuppressLint("SimpleDateFormat")
     private fun observe() {
         viewModel.priorityList.observe(this) {
             listPriority = it
@@ -99,11 +123,32 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener,
 
         viewModel.taskSave.observe(this) {
             if (it.status()) {
-                toast("Sucesso")
+                if(taskIdentification == 0) {
+                    toast("Tarefa criada com sucesso.")
+                }
+                else{
+                    toast("Tarefa atualizada com sucesso.")
+                }
                 finish()
             } else {
                 toast(it.message())
             }
         }
+
+        viewModel.task.observe(this) {
+            binding.editDescription.setText(it.description)
+            binding.checkComplete.isChecked = it.complete
+            binding.spinnerPriority.setSelection(getIndex(it.priorityId))
+            val date = SimpleDateFormat("yyyy-MM-dd").parse(it.dueDate)
+            binding.buttonDate.text = SimpleDateFormat("dd/MM/yyyy").format(date)
+        }
+
+        viewModel.taskLoad.observe(this) {
+            if (!it.status()) {
+                toast(it.message())
+                finish()
+            }
+        }
     }
+
 }
