07d8ff9c1ac99b67bb93c166f9b7a5d466ec74ed
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-02-10 12:40:58 -0300

    Implementação: Implementado a lógica para criação de novo usuário através da própria API na classe PersonRepository.kt

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
@@ -4,15 +4,10 @@ import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.devmasterteam.tasks.R
-import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.repository.remote.PersonService
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
-import com.google.gson.Gson
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class PersonRepository(context: Context) : BaseRepository(context) {
 
@@ -25,5 +20,14 @@ class PersonRepository(context: Context) : BaseRepository(context) {
             return
         }
         executeCall(remote.login(email, password), listener)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun create(name: String, email: String, password: String, listener: APIListener<PersonModel>) {
+        if (!isConnectionAvaliable()) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+        executeCall(remote.create(name, email, password), listener)
     }
 }
