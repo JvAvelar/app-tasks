567ab86fbbcc17068c7fe997104e7538f1f6d395
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-05-31 23:29:49 -0300

    Alterado o observer do LiveData para collect do Flow e adicionando em threads

--- app/src/main/java/com/devmasterteam/tasks/view/AllTasksFragment.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/AllTasksFragment.kt
@@ -1,21 +1,23 @@
 package com.devmasterteam.tasks.view
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devmasterteam.tasks.databinding.FragmentAllTasksBinding
 import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.TaskListener
 import com.devmasterteam.tasks.view.adapter.TaskAdapter
 import com.devmasterteam.tasks.viewmodel.TaskListViewModel
+import kotlinx.coroutines.launch
 
 class AllTasksFragment : Fragment() {
 
@@ -24,16 +26,16 @@ class AllTasksFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val adapter = TaskAdapter()
-    private var taskFilter = 0
+    private var taskFilter = TaskConstants.FILTER.ALL
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View {
-        viewModel = ViewModelProvider(this).get(TaskListViewModel::class.java)
+        viewModel = ViewModelProvider(this)[TaskListViewModel::class.java]
         _binding = FragmentAllTasksBinding.inflate(inflater, container, false)
 
         binding.recyclerAllTasks.layoutManager = LinearLayoutManager(context)
         binding.recyclerAllTasks.adapter = adapter
 
-       taskFilter = requireArguments().getInt(TaskConstants.BUNDLE.TASKFILTER, 0)
+        taskFilter = requireArguments().getInt(TaskConstants.BUNDLE.TASKFILTER, 0)
 
         val listener = object : TaskListener {
             override fun onListClick(id: Int) {
@@ -44,24 +46,20 @@ class AllTasksFragment : Fragment() {
                 startActivity(intent)
             }
 
-            @RequiresApi(Build.VERSION_CODES.M)
             override fun onDeleteClick(id: Int) {
                 viewModel.delete(id)
             }
 
-            @RequiresApi(Build.VERSION_CODES.M)
             override fun onCompleteClick(id: Int) {
                 viewModel.status(id, true)
             }
 
-            @RequiresApi(Build.VERSION_CODES.M)
             override fun onUndoClick(id: Int) {
                 viewModel.status(id, false)
             }
         }
 
         adapter.attachListener(listener)
-
 
         // Cria os observadores
         observe()
@@ -74,25 +72,28 @@ class AllTasksFragment : Fragment() {
         _binding = null
     }
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onResume() {
         super.onResume()
         viewModel.list(taskFilter)
     }
 
     private fun observe() {
-        viewModel.tasks.observe(viewLifecycleOwner) {
-            adapter.updateTasks(it)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.tasks.collect { value ->
+                    adapter.updateTasks(value)
+                }
+            }
         }
+
         viewModel.delete.observe(viewLifecycleOwner) {
             if (!it.status())
                 Toast.makeText(context, it.message(), Toast.LENGTH_SHORT).show()
         }
 
         viewModel.status.observe(viewLifecycleOwner) {
-            if (!it.status()) {
+            if (!it.status())
                 Toast.makeText(context, it.message(), Toast.LENGTH_SHORT).show()
-            }
         }
     }
 }
