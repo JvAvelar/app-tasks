41365961b3f3c9715ad944da7a4649f1334864e6
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-05-31 23:25:02 -0300

    Implementado o método list na PriorityRepository

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
@@ -1,33 +1,17 @@
 package com.devmasterteam.tasks.service.repository
 
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import com.devmasterteam.tasks.R
-import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.repository.local.TaskDatabase
 import com.devmasterteam.tasks.service.repository.remote.PriorityService
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
-import com.google.gson.Gson
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class PriorityRepository(context: Context) : BaseRepository(context) {
 
     private val remote = RetrofitClient.getService(PriorityService::class.java)
     private val database = TaskDatabase.getDatabase(context).priorityDAO()
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun list(listener: APIListener<List<PriorityModel>>) {
-        if (!isConnectionAvaliable()) {
-            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
-            return
-        }
-        executeCall(remote.list(), listener)
-    }
 
     // Cache
     companion object {
@@ -39,6 +23,14 @@ class PriorityRepository(context: Context) : BaseRepository(context) {
         fun setDescription(id: Int, str: String) {
             cache[id] = str
         }
+    }
+
+    fun list(listener: APIListener<List<PriorityModel>>) {
+        if (!isConnectionAvaliable()) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+        executeCall(remote.list(), listener)
     }
 
     fun getDescription(id: Int): String {
@@ -55,7 +47,6 @@ class PriorityRepository(context: Context) : BaseRepository(context) {
     fun list(): List<PriorityModel> {
         return database.list()
     }
-
 
     fun save(list: List<PriorityModel>) {
         database.clear()
