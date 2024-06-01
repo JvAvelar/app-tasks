368e7daabcb5dc49b063ff0311d36c4be57c226b
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-05-31 23:28:26 -0300

    Alterado o observer do LiveData para collect do Flow e adicionando em threads

--- app/src/main/java/com/devmasterteam/tasks/view/MainActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/MainActivity.kt
@@ -3,26 +3,30 @@ package com.devmasterteam.tasks.view
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.ui.NavigationUI
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.databinding.ActivityMainBinding
 import com.devmasterteam.tasks.viewmodel.MainViewModel
+import com.google.android.material.navigation.NavigationView
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit  var viewModel: MainViewModel
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,10 +51,6 @@ class MainActivity : AppCompatActivity() {
         observe()
     }
 
-    override fun onResume() {
-        super.onResume()
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
@@ -67,11 +67,9 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
         navView.setNavigationItemSelectedListener {
-            if (it.itemId == R.id.nav_logout) {
-                viewModel.logout()
-                startActivity(Intent(applicationContext, LoginActivity::class.java))
-                finish()
-            } else {
+            if (it.itemId == R.id.nav_logout)
+                logout()
+            else {
                 NavigationUI.onNavDestinationSelected(it, navController)
                 drawerLayout.closeDrawer(GravityCompat.START)
             }
@@ -79,10 +77,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun logout() {
+        viewModel.logout()
+        startActivity(Intent(applicationContext, LoginActivity::class.java))
+        finish()
+    }
+
     private fun observe() {
-        viewModel.name.observe(this){
-            val header = binding.navView.getHeaderView(0)
-            header.findViewById<TextView>(R.id.text_name).text = it
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.name.collect { value ->
+                    val header = binding.navView.getHeaderView(0)
+                    header.findViewById<TextView>(R.id.text_name).text = value
+                }
+            }
         }
     }
 }
